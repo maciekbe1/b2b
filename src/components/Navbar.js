3007d831dea1395f190ce92f5e5ac444bbb22b42
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2020-12-08 10:16:09 +0100

    center social media icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -102,7 +102,7 @@ export default function Navbar(props) {
               </div>
             </div>
             <div className="flex align-middle justify-center absolute right-0 flex-col bottom-3">
-              <div className="flex justify-center items-center">
+              <div className="flex justify-center">
                 <div className="sm:hidden lg:block">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -114,29 +114,31 @@ export default function Navbar(props) {
                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                   </svg>
                 </div>
-                <p className="ml-2 text-md text-navy font-bold sm:hidden lg:block">
-                  <span className="text-logoGreen">+48 </span>510-510-499
-                </p>
-              </div>
-              <div className="flex justify-center sm:hidden lg:flex">
-                <button
-                  className="bg-white text-blue-600 font-normal h-10 w-10 items-center justify-center align-center outline-none focus:outline-none p-3"
-                  type="button"
-                >
-                  <img src={facebook} alt="" />
-                </button>
-                <button
-                  className="bg-white text-pink-400 font-normal h-10 w-10 items-center justify-center align-center outline-none focus:outline-none p-3"
-                  type="button"
-                >
-                  <img src={instagram} alt="" />
-                </button>
-                <button
-                  className="bg-white text-pink-400 font-normal h-10 w-10 items-center justify-center align-center outline-none focus:outline-none p-3"
-                  type="button"
-                >
-                  <img src={youtube} alt="" />
-                </button>
+                <div className="ml-2 sm:hidden lg:block">
+                  <p className="text-md text-navy font-bold ">
+                    <span className="text-logoGreen">+48 </span>510 510 499
+                  </p>
+                  <div className="flex justify-center sm:hidden lg:flex">
+                    <button
+                      className="bg-white text-blue-600 font-normal h-10 w-10 items-center justify-center align-center outline-none focus:outline-none p-3"
+                      type="button"
+                    >
+                      <img src={facebook} alt="" />
+                    </button>
+                    <button
+                      className="bg-white text-pink-400 font-normal h-10 w-10 items-center justify-center align-center outline-none focus:outline-none p-3"
+                      type="button"
+                    >
+                      <img src={instagram} alt="" />
+                    </button>
+                    <button
+                      className="bg-white text-pink-400 font-normal h-10 w-10 items-center justify-center align-center outline-none focus:outline-none p-3"
+                      type="button"
+                    >
+                      <img src={youtube} alt="" />
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
